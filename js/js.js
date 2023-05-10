04fadb8fa053fdafda92e46b4c03ed67752798ac
Jhonny Cuauro <yonnycuauro365@gmail.com>
2023-05-10 10:32:00 -0400

    porbando

--- js/js.js
+++ js/js.js
@@ -8,37 +8,28 @@ function img(){
   let imagen= document.createElement("img")
   imagen.src= "css/li.png";
   imagen.id= "imagen"
-  const bodySize = document.querySelector('body')
+  // const bodySize = document.querySelector('body')
 
-  let y = Math.floor (Math.random()*bodySize.clientWidth)*0.9;
-  let x = Math.floor (Math.random()*bodySize.clientWidth)*0.8;
+  // let y = Math.floor (Math.random()*bodySize.clientWidth)*0.9;
+  // let x = Math.floor (Math.random()*bodySize.clientWidth)*0.8;
+  let y = Math.floor()*100;
+  let x = Math.floor()*100;
   imagen.style.rotate= Math.floor (Math.random()*360) + "deg";
 
   imagen.width= Math.floor(Math.random()*100);
   imagen.height= imagen.width;
   
-  if ( (y >= 200 && y <= 400) && (x >= 300 && x <= 700) ) {
+  if ( (y >= 45 && y <= 50) && (x >= 51 && x <= 55) ) {
     console.log("1")
 
-    if ( y >= 250 && y <= 300 ) {
-      imagen.style.top= (y - 100) + "px";
-    }
-    else {
-      imagen.style.top= (y + 100) + "px";
-    }
-    imagen.style.left= x + "%"
-    // imagen.width= 10;
-    imagen.style.border='solid white 2px'
+    imagen.width= 10;
     imagen.height= imagen.width;
+    imagen.style.border='solid blue 2px'
     body.appendChild(imagen)
     
   }
-
-  else {
-    imagen.style.top= y + "px"
-    imagen.style.left= x + "px";
-    body.appendChild(imagen)
-    console.log("2")
+  else{
+    y= y + "%"
+    x= x + "%"
   }
-
 }
